746c50b47ff351415485995be99301773cc7829f
Ashish Kumar Rout <42204416+F13RC3@users.noreply.github.com>
2023-12-12 15:51:59 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull f13rc3/simple-python-flask-app
+docker pull f13rc3/simple_python_flask_app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 f13rc3/simple-python-flask-app
+docker run -d -p 5000:5000 f13rc3/simple_python_flask_app
